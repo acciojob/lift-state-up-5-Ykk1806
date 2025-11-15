ae00736ef8cbef97a23901b4be7df98ba6a11e79
Yogesh <yogeshkhatake18@gmail.com>
2025-11-16 00:16:45 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-
+import { useState } from "react";
 import React from "react";
-import './../styles/App.css';
-
+import LoginForm from "./LoginForm";
 const App = () => {
+  const [isLoggedIn,setIsLoggedIn]=useState(false)
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        {
+          isLoggedIn ? (<h2>You are Logged In!</h2>):(<LoginForm  setIsLoggedIn={setIsLoggedIn}/>)
+        }
+
     </div>
   )
 }
